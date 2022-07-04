5abc006c7bb571aca3ab183cff6368190c8946d8
Andres <afarangurens@unal.edu.co>
2022-07-04 16:17:30 -0500

    On root print hello express

--- myApp.js
+++ myApp.js
@@ -3,37 +3,21 @@ let app = express();
 
 console.log("Hello World");
 
+// Routes in Express take the following structure:
+// app.METHOD(PATH, HANDLER)
+// Where METHOD is an http request
+// PATH is a relative path on the server str or RE
+// HANDLER is a function that Express calls when the reoute is matched
+// Handlers take the form function(req, res){...}
 
+// handler example:
 
+// function(req, res){
+//     res.send('Response String');
+// }
 
+app.get('/', (req, res)=>{
+    res.send('Hello Express');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
